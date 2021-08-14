a40044686ac1729a50202fd418ff38b6a604bc36
Noreen Wu <noreenwu@gmail.com>
2021-08-13 21:32:36 -0400

    configuration changes after exposing frontend and reverseproxy services

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1c504a034010473e97b2cd335fc45ff-1604091304.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
