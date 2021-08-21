0eac938f9090235be886e34f015b4d6a4037c271
Noreen Wu <noreenwu@gmail.com>
2021-08-21 12:05:56 -0400

    removed debug code from udagram-api-feed and change docker container version to v22

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -4,7 +4,7 @@ import {config} from './config/config';
 
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
-AWS.config.credentials = credentials;
+console.log("credentials from the file are ", credentials)
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
